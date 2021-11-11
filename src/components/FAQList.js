6cf2b5428fec39c979f4e746cf606444c3920986
ahfaisalkarimi <ahfaisalkarimi@gmail.com>
2021-11-11 11:55:09 -0800

    Fix styling

--- src/components/FAQList.js
+++ src/components/FAQList.js
@@ -3,9 +3,14 @@ import FAQ from "./FAQ";
 import { sampleQestions } from "../questions/sampleQuestions";
 
 const FAQList = () => {
-  return sampleQestions.map((question) => (
-    <FAQ question={question.question} answer={question.answer} />
-  ));
+  return (
+    <>
+      <h1>Sample FAQ</h1>
+      {sampleQestions.map((question) => (
+        <FAQ question={question.question} answer={question.answer} />
+      ))}
+    </>
+  );
 };
 
 export default FAQList;
